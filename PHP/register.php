7ab76fb6ad7688e569aa07c28b8508a89b025a85
TLeborgne <leborgne@et.esiea.fr>
2017-09-05 13:51:15 +0200

    added register.php

---
+++ PHP/register.php
@@ -0,0 +1,23 @@
+<?php
+    $dsn = 'mysql:dbname=chatbox;localhost';
+    $dbuser = 'root';
+    $pdo = new PDO($dsn, $dbuser);
+
+    $nickname = trim($_GET['nickname']);
+    $email = trim($_GET['email']);
+    $password = trim($_GET['nickname']);
+
+    if ($nickname && $email && $password) {
+        $password = sha1($password);
+        $req = $pdo->prepare('INSERT INTO `user` (nickname, email, password, last_logout) VALUES (:nickname, :email, :password, NOW())');
+        $results = $req->execute(array(':nickname' => $nickname, ':email' => $email, ':password' => $password));
+        if ($results) {
+            echo ('success' => 'true');
+            return true;
+        } else {
+            echo ('success' => 'false', 'error' => 'Erreur lors de l\'entrée en BDD.');
+        }
+    } else {
+        echo ('success' => 'false', 'error' => 'Veuillez renseigner tous les champs.');
+    }
+?>
